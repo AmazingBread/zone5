42cb956992b7a8fcaf699c7984c56d5a108c12a8
bread_peaty <83941202+AmazingBread@users.noreply.github.com>
2025-03-25 14:21:50 +0900

    배너 주석

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -36,14 +36,15 @@
         <!--    controls-->
         <!--&gt;-->
         <!--</video>-->
-        <!--<div class="mb-3" @click="$router.push('/group_buying')" style="cursor: pointer;">-->
-        <!--    <img src="@/assets/image/yohemite.png" class="img-fluid" alt="buying" />-->
-        <!--</div>-->
-        <!--<p style="text-align: center; padding: 10px;">-->
-        <!--    📣📣요헤미티 할인코드📣📣<br>-->
-        <!--    1. 개인구매 10프로 할인코드 - DM 문의바람<br>-->
-        <!--    2. 단채구매 40프로 이상 할인 - 12월 5일 마감<br>-->
-        <!--</p>-->
+        <div class="mb-3" @click="$router.push('/group_buying')" style="cursor: pointer;">
+            <img src="@/assets/image/yohemite.png" class="img-fluid" alt="buying" />
+        </div>
+        <p style="text-align: center; padding: 10px;">
+            요헤미티 공동구매
+            <!--📣📣요헤미티 할인코드📣📣<br>-->
+            <!--1. 개인구매 10프로 할인코드 - DM 문의바람<br>-->
+            <!--2. 단채구매 40프로 이상 할인 - 12월 5일 마감<br>-->
+        </p>
         <!--<div class="mb-3" @click="$router.push('/bear')" style="cursor: pointer;">-->
         <!--    <img src="@/assets/image/bear_main.jpg" class="img-fluid" alt="썸네일" />-->
         <!--</div>-->
